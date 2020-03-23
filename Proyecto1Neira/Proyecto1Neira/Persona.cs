7ce088fb298fae90ebe2545343b4ba9ef4ac38a4
Cristobal <c@iMac-de-Rene.local>
2020-03-23 20:21:25 -0300

    Tercer commit

--- Proyecto1Neira/Proyecto1Neira/Persona.cs
+++ Proyecto1Neira/Proyecto1Neira/Persona.cs
@@ -3,8 +3,32 @@ namespace Proyecto1Neira
 {
     public class Persona
     {
-        public Persona()
+        private string nombre;
+        private string apellido;
+
+        public Persona(string nombre, string apellido)
         {
+            this.nombre = nombre;
+            this.apellido = apellido;
         }
+
+        public string GetName()
+        {
+            return nombre;
+        }
+
+        public string GetApe()
+        {
+            return apellido;
+        }
+
+        public int Lanzar()
+        {
+            Random rdn = new Random();
+            int a = rdn.Next(0, 3);
+            return a;
+        }
+
     }
+    
 }
--- Proyecto1Neira/Proyecto1Neira/Program.cs
+++ Proyecto1Neira/Proyecto1Neira/Program.cs
@@ -2,11 +2,15 @@
 
 namespace Proyecto1Neira
 {
+
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Persona person1 = new Persona("Bob", "Kunga");
+            Console.WriteLine("Nombre: " + person1.GetName());
+            Console.WriteLine("Apellido: " + person1.GetApe());
+            Console.WriteLine("Persona lanzo: " + person1.Lanzar());
         }
     }
 }
